b5cb556caa5b84a041dd9dbf3f2e2245656f8813
ducnm13 <ducnm.contact@gmail.com>
2025-09-05 11:01:31 +0700

    add mysql

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const { engine } = require("express-handlebars");
 const path = require("path");
 const morgan = require("morgan");
 const app = express();
+const mysql = require("mysql2/promise");
 const port = 3000;
+
+// Connect Database
+async function connectDB() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+  console.log("✅ Connected to MySQL");
+  const [rows] = await connection.execute("SELECT * FROM tbl_nhanvien");
+  console.log(rows);
+}
+
+connectDB();
+// end
 
 const route = require("./routes");
 
